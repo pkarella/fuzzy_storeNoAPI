88e3fd3c7a81de4b9fe1b48831675b6e759a1a60
Peter Karellas and Starlynne Start <peter+starlynne@>
2017-07-12 09:59:22 -0700

    create review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170711210432) do
+ActiveRecord::Schema.define(version: 20170712164703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 20170711210432) do
     t.string "description"
     t.decimal "price"
     t.integer "inventory"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "author"
+    t.string "content"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
